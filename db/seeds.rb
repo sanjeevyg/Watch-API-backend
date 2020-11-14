483eb956aca76ee5a0bf7ae13d40730bc5da96b5
sanjeevyg <yogi.sjv@gmail.com>
2020-11-13 17:32:03 -0700

    backend-revised

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,26 @@
 
 
 user1 = User.create(name: "Albert", email: "albert@gmail.com", password: "albert123", username: "albert_1")
+user2 = User.create(name: "Sanjeev Yogi", email: "yogi.sjv@gmail.com", password: "Laxmi123", username: "sanjeevyg")
 
-watch1 = Watch.create(brand: "MVMT", image: "https://ibb.co/pRSPvzP", interchangeable_strap: "Yes", dial_color: "Navy blue", case_material: "Stainless Steel", price: 159.90)
-watch2 = Watch.create(brand: "MVMT", image: "https://ibb.co/fCt0PL6", interchangeable_strap: "No", dial_color: "White", case_material: "Stainless Steel", price: 114.75)
-watch3 = Watch.create(brand: "MVMT", image: "https://ibb.co/f0zWwPC", interchangeable_strap: "No", dial_color: "Black", case_material: "Rose Gold Ionic-Plated Stainless Steel", price: 87.40)
-watch4 = Watch.create(brand: "Bering", image: "https://ibb.co/289bWSz", interchangeable_strap: "No", dial_color: "Dark brown", case_material: "Brass", price: 49)
-watch5 = Watch.create(brand: "Bering", image: "https://ibb.co/pPhmt1D", interchangeable_strap: "No", dial_color: "White", case_material: "Stainless Steel", price: 299)
-watch6 = Watch.create(brand: "Bering", image: "https://ibb.co/YXngZ4F", interchangeable_strap: "Yes", dial_color: "Gald", case_material: "Titanium", price: 129)
-watch7= Watch.create(brand: "MVMT", image: "https://ibb.co/x3fn13L", interchangeable_strap: "Yes", dial_color: "Blue and black", case_material: "Nylon", price: 49)
-watch8 = Watch.create(brand: "VTEK", image: "https://ibb.co/WGmdBzS", interchangeable_strap: "Yes", dial_color: "Black", case_material: "Stainless Steel", price: 99)
-watch9 = Watch.create(brand: "TAG Heuer", image: "https://ibb.co/zrFw27G", interchangeable_strap: "No", dial_color: "Blue", case_material: "Stainless Steel", price: 299)
+watch1 = Watch.create(brand: "MVMT", image: "https://i.postimg.cc/VvXq2Cjy/w1.jpg", interchangeable_strap: "Yes", dial_color: "Navy blue", case_material: "Stainless Steel", price: 159.90)
+watch2 = Watch.create(brand: "MVMT", image: "https://i.postimg.cc/T3dVHh1S/w2.jpg", interchangeable_strap: "No", dial_color: "White", case_material: "Stainless Steel", price: 114.75)
+watch4 = Watch.create(brand: "Bering", image: "https://i.postimg.cc/vmStnMQK/w4.jpg", interchangeable_strap: "No", dial_color: "Dark brown", case_material: "Brass", price: 49.00)
+watch5 = Watch.create(brand: "Bering", image: "https://i.postimg.cc/kMSv2kLj/w5.jpg", interchangeable_strap: "No", dial_color: "White", case_material: "Stainless Steel", price: 299.00)
+watch6 = Watch.create(brand: "Bering", image: "https://i.postimg.cc/Gh1JGXCb/w6.jpg", interchangeable_strap: "Yes", dial_color: "Gald", case_material: "Titanium", price: 129.00)
+watch3 = Watch.create(brand: "MVMT", image: "https://i.postimg.cc/J4c3qjrZ/w3.jpg", interchangeable_strap: "No", dial_color: "Black", case_material: "Gold-Plated SS", price: 87.40)
+watch7= Watch.create(brand: "MVMT", image: "https://i.postimg.cc/DZPrSLjv/w7.jpg", interchangeable_strap: "No", dial_color: "Silver Sunray", case_material: "Stainless Steel", price: 49.29)
+watch8 = Watch.create(brand: "MVMT", image: "https://i.postimg.cc/Hkm7yVMj/w8.jpg", interchangeable_strap: "Yes", dial_color: "Black", case_material: "Stainless Steel", price: 99.12)
+watch9 = Watch.create(brand: "TAG Heuer", image: "https://i.postimg.cc/bwYwPbb0/w9.jpg", interchangeable_strap: "No", dial_color: "White", case_material: "Milanese", price: 299.00)
+watch11 = Watch.create(brand: "Bering", image: "https://i.postimg.cc/vZGd9sZG/w11.jpg", interchangeable_strap: "No", dial_color: "Blue Sunray", case_material: "Stainless Steel", price: 189.00)
+watch11 = Watch.create(brand: "MVMT", image: "https://i.postimg.cc/rmD9PKQM/w12.jpg", interchangeable_strap: "No", dial_color: "Blue", case_material: "Stainless Steel", price: 80.75)
 
+# https://i.postimg.cc/VvXq2Cjy/w1.jpg
+# https://i.postimg.cc/T3dVHh1S/w2.jpg
+# https://i.postimg.cc/J4c3qjrZ/w3.jpg
+# https://i.postimg.cc/vmStnMQK/w4.jpg
+# https://i.postimg.cc/kMSv2kLj/w5.jpg
+# https://i.postimg.cc/Gh1JGXCb/w6.jpg
+# https://i.postimg.cc/Znz3P5Xw/w7.png
+# https://i.postimg.cc/PJbDSkjT/w8.png
+# https://i.postimg.cc/J4mZGtNn/w9.png
